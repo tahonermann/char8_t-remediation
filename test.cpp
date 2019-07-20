5780d8f613a495baac5d0a3aafd299f60d56a93a
Tom Honermann <tom@honermann.net>
2019-07-21 01:10:58 +0200

    Reworked #ifdef's for test warnings.

--- test.cpp
+++ test.cpp
@@ -18,13 +18,11 @@ void test1() {
     const char *ps2 = U8("concatenated" "text");
 #if !defined(__cpp_char8_t) || defined(__cpp_nontype_template_parameter_class)
     const char (&rac5)[5] = U8("text");
-#else
+#elif __cplusplus >= 202002L // FIXME: Guessing at the C++20 value
+    // Don't warn when compiling for C++17 or earlier.
     #warning Not testing binding references to array to string literals.
 #endif
 
-#if __cplusplus < 202002L // FIXME: Guessing at the C++20 value
-    // Don't warn if testing C++11 or earlier.
-#else
 #if !defined(__GNUC__) // Includes Clang
     // gcc allowed conversions to reference to array of unknown bound in version
     // 9.0.0 20181027, but no longer does so as of 9.0.1 20190201.
@@ -32,9 +30,9 @@ void test1() {
     // but neither gcc nor Clang implement it yet.
     // FIXME: Enable this test when gcc and/or Clang implements P0388R4.
     const char (&rac)[] = U8("text");
-#else
+#elif __cplusplus >= 202002L // FIXME: Guessing at the C++20 value
+    // Don't warn when compiling for C++17 or earlier.
     #warning Not testing binding references to array of unknown bound to string literals.
-#endif
 #endif
 }
 
@@ -52,13 +50,11 @@ void test_constexpr() {
     constexpr const char *ps = U8("text");
     constexpr const char *ps2 = U8("concatenated" "text");
     constexpr const char (&rac5)[5] = U8("text");
-#else
+#elif __cplusplus >= 202002L // FIXME: Guessing at the C++20 value
+    // Don't warn when compiling for C++17 or earlier.
     #warning Not testing constexpr initialization of pointers with string literals.
 #endif
 
-#if __cplusplus < 202002L // FIXME: Guessing at the C++20 value
-    // Don't warn if testing C++11 or earlier.
-#else
 #if !defined(__GNUC__) // Includes Clang
     // gcc allowed conversions to reference to array of unknown bound in version
     // 9.0.0 20181027, but no longer does so as of 9.0.1 20190201.
@@ -66,15 +62,13 @@ void test_constexpr() {
     // but neither gcc nor Clang implement it yet.
     // FIXME: Enable this test when gcc and/or Clang implements P0388R4.
     constexpr const char (&rac)[] = U8("text");
-#else
+#elif __cplusplus >= 202002L // FIXME: Guessing at the C++20 value
+    // Don't warn when compiling for C++17 or earlier.
     #warning Not testing constexpr binding references to array of unknown bound to string literals.
-#endif
 #endif
 }
 
-#if __cplusplus < 201402L
-    // Don't warn if testing C++11 or earlier.
-#else
+#if __cplusplus >= 201402L // char_array requires C++14.
 #if !defined(__cpp_char8_t) || defined(__cpp_nontype_template_parameter_class)
 #if defined(__cpp_deduction_guides)
 constexpr char_array ca1 = U8("text");
@@ -89,15 +83,14 @@ constexpr char c2 = ca2[0];
 constexpr char_array<7> ca3 = U8("text");
 constexpr const char(&rca3)[7] = ca3;
 constexpr char c3 = ca3[0];
-#else
+#elif __cplusplus >= 202002L // FIXME: Guessing at the C++20 value
+    // Don't warn when compiling for C++17 or earlier.
     #warning Not testing constexpr array initialization with string literals.
 #endif
 #endif
 
 void test_array_init() {
-#if __cplusplus < 201402L
-    // Don't warn if testing C++11 or earlier.
-#else
+#if __cplusplus >= 201402L // char_array requires C++14.
 #if !defined(__cpp_char8_t) || defined(__cpp_nontype_template_parameter_class)
 #if defined(__cpp_deduction_guides)
     constexpr char_array ca1 = U8("text");
@@ -109,7 +102,8 @@ void test_array_init() {
     constexpr char c2 = ca2[0];
     constexpr char_array<7> ca3 = U8("text");
     constexpr char c3 = ca3[0];
-#else
+#elif __cplusplus >= 202002L // FIXME: Guessing at the C++20 value
+    // Don't warn when compiling for C++17 or earlier.
     #warning Not testing constexpr array initialization with string literals.
 #endif
 #endif
